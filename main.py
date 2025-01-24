6479395c9fb3df9849884b705c578c43e02ed80a
Pranav-Pote-28 <129758900+Pranav-Pote-28@users.noreply.github.com>
2025-01-25 01:24:37 +0530

    commit 2501

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import JSONResponse
 
 app=FastAPI()
 
-@app.get("/")
-async def root():
-    return ({"message":"hello world"})
+# @app.get("/")
+# async def root():
+#     return ({"message":"hello world"})
+
+@app.post("/")
+async def handle_request(request:Request):
+    payload=await request.json()
+
+    intent=payload["queryResult"]["intent"]["displayName"]
+    parameters=payload["queryResult"]["parameters"]
+    outputContexts=payload["queryResult"]["outputContexts"]
+
+    if intent=="track.order-context:ongoing-tracking":
+
+        return JSONResponse(content={"fulfillmentText":"received intent track.order in the backend !"})

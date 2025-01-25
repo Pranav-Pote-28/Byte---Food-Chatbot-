7e4f8d38f683c48acb75432370875c8f35d8d3a2
Pranav-Pote-28 <129758900+Pranav-Pote-28@users.noreply.github.com>
2025-01-26 01:04:16 +0530

    2601_commit

---
+++ db_helper.py
@@ -0,0 +1,27 @@
+import mysql.connector
+global cnx
+
+cnx=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="pandeyji_eatery"
+)
+
+def get_order_status(order_id :int):
+    cursor=cnx.cursor()
+
+    query=("SELECT status FROM order_tracking WHERE order_id=%s")
+    cursor.execute(query,(order_id,))
+
+    result=cursor.fetchone()
+    cursor.close()
+    # cnx.close()
+
+    if result is not None:
+        return result[0]
+    else:
+        return None
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
+import db_helper
+import generic_helper
 
 app=FastAPI()
 
 # @app.get("/")
 # async def root():
 #     return ({"message":"hello world"})
+
+inprogress_orders={}
 
 @app.post("/")
 async def handle_request(request:Request):
@@ -16,6 +20,53 @@ async def handle_request(request:Request):
     parameters=payload["queryResult"]["parameters"]
     outputContexts=payload["queryResult"]["outputContexts"]
 
-    if intent=="track.order-context:ongoing-tracking":
+    session_id=generic_helper.extract_session_id(outputContexts[0]["name"])
 
-        return JSONResponse(content={"fulfillmentText":"received intent track.order in the backend !"})
+    intent_handler_dict={
+        "order.add-context: ongoing-order":add_to_order,
+        # "order.complete-context:ongoing-order":remove_from_order,
+        # "order.remove-context: ongoing-order":complete_order,
+        "track.order-context:ongoing-tracking":track_order
+    }
+
+    return intent_handler_dict[intent](parameters,session_id)
+
+
+
+
+def add_to_order(parameters:dict,session_id:str):
+    food_items=parameters["food-item"]
+    quantities=parameters["number"]
+
+    if len(food_items)!=len(quantities):
+        fulfillmenttext="Please enter the quantity and fooditems respectively"
+    else:
+        new_food_dict=dict(zip(food_items,quantities))
+
+        if session_id in inprogress_orders:
+            current_food_dict=inprogress_orders[session_id]
+            current_food_dict.update(new_food_dict)
+            inprogress_orders[session_id]=current_food_dict
+        else:
+            inprogress_orders[session_id]=new_food_dict
+            print(inprogress_orders,session_id)
+
+        order_str=generic_helper.get_str_from_food_dict(inprogress_orders[session_id])
+
+
+        fulfillmenttext=f"so far you have ordered {order_str}"
+
+    return JSONResponse(content={"fulfillmentText":fulfillmenttext})
+
+
+
+def track_order(parameters:dict):
+    order_id=int(parameters["order_id"])
+    order_status=db_helper.get_order_status(order_id)
+
+    if order_status:
+        fulfillmenttext=f"Order Status: {order_status} for OrderId: {order_id}"
+    else:
+        fulfillmenttext=f"No order found with OrderId: {order_id}"
+
+    return JSONResponse(content={"fulfillmentText":fulfillmenttext})
